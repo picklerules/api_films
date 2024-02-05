0b7a0e7aaa75496dd7b9edb4c456ecc4c5b8b26d
picklerules <136508588+picklerules@users.noreply.github.com>
2024-02-05 16:35:22 -0500

    initiliser la DB utilisateurs

--- app.js
+++ app.js
@@ -89,7 +89,10 @@ server.get('/api/films/:id', async (req, res) =>{
 })
   
 
-//Initialiser les données films
+/**
+ * @method POST
+ * Permet d'initialiser la base de donnée
+ */
 server.post('/api/films/initialiser',(req,res)=>{
    
     const donneesTest = require('./data/filmsTest.js');
@@ -102,7 +105,27 @@ server.post('/api/films/initialiser',(req,res)=>{
     res.json({ message : 'La base de donnée a été initialisée avec les films.' });
 });
 
-//REQUÊTE POST  
+/**
+ * @method POST 
+ * Permet d'initialiser la base de donnée
+ */
+server.post('/api/utilisateurs/initialiser',(req,res)=>{
+   
+    const donneesTest = require('./data/utilisateurTest.js');
+
+    donneesTest.forEach(async(element)=>{
+        await db.collection('utilisateurs').add(element);
+    });
+
+    res.statusCode = 200;
+    res.json({ message : 'La base de donnée a été initialisée avec les utilisateurs.' });
+});
+
+
+/**
+ * @method POST
+ * Permet d'ajouter un film
+ */
 server.post('/api/films', async (req, res)=>{
 
     try {
@@ -119,7 +142,7 @@ server.post('/api/films', async (req, res)=>{
         const newFilm = await db.collection('films').add(film);
 
     res.statusCode = 201;
-    res.json({ message : 'Le film a été ajoutée', id: newFilm.id, donnees: film});
+    res.json({ message : 'Le film a été ajouté', id: newFilm.id, donnees: film});
 
     } catch (e) {
 
@@ -129,18 +152,151 @@ server.post('/api/films', async (req, res)=>{
     }
 });
 
-// //Initialiser les données utilisateur
-// server.post('/api/utilisateur/initialiser',(req,res)=>{
-   
-//     const donneesTest = require('./data/utilisateurTest.js');
+/**
+ * @method POST
+ * Permet d'ajouter un utilisateur / inscrire un utilisateur
+ */
+server.post('/api/utilisateurs/inscription', async (req, res)=>{
 
-//     donneesTest.forEach(async(element)=>{
-//         await db.collection('films').add(element);
-//     });
+    try {
+        const utilisateur = req.body;
+        // console.log(test);
 
-//     res.statusCode = 200;
-//     res.json({ message : 'La base de donnée a été initialisée avec les utilisateurs.' });
-// });
+        //validation des données
+        if(utilisateur.courriel == undefined || utilisateur.mdp == undefined) {
+
+            res.statusCode = 400;
+            return res.json({ message: 'Veuillez remplir les informations.' });
+        }
+        
+        //vérifier si l'utilisateur existe déjà
+        const utilisateurExiste = await db.collection('utilisateurs').where('courriel', '==', utilisateur.courriel).get();
+
+        if(utilisateurExiste.docs.length > 0) {
+
+            res.statusCode = 400;
+            return res.json({ message: 'L\'utilisateur existe déjà' });
+        }
+
+
+        const newUtilisateur = await db.collection('utilisateurs').add(utilisateur);
+
+    res.statusCode = 201;
+    res.json({ message : 'L\'utilisateur a été ajouté', id: newUtilisateur.id});
+
+    } catch (e) {
+
+        res.statusCode = 500;
+        res.json({ message: 'Erreur lors de l\'ajout de l\'utilisateur' });
+
+    }
+});
+
+
+/**
+ * @method POST
+ * Permet la connexion d'un utilisateur
+ */
+server.post('/api/utilisateurs/connexion', async (req, res)=>{
+
+  try {
+
+    const {courriel, mdp} = req.body;
+
+    //vérifier si le courriel de l'utilisateur existe dans la DB
+    const utilisateurConnexion = await db.collection('utilisateurs').where('courriel', '==', courriel).get();
+
+    //comparer le mdp avec la DB
+    if(utilisateurConnexion.docs.length > 0) {
+
+        //recuperer l'utilisateur connecté
+        const utilisateur = utilisateurConnexion.docs[0].data();
+
+        if(mdp == utilisateur.mdp) {
+
+            res.statusCode = 200;
+            res.json({ message: 'Connexion réussie', courriel: utilisateur.courriel});
+
+        } else {
+
+            res.statusCode = 400;
+            res.json({ message: 'Mot de passe incorrect' });
+
+        }
+
+    } else {
+
+        res.statusCode = 400;
+        res.json({ message: 'Utilisateur non trouvé' });
+
+    }
+
+  } catch (e) {
+
+    res.statusCode = 500;
+    res.json({ message: 'Erreur lors de la connexion' });
+
+  }
+
+
+
+
+})
+
+/**
+ * @method PUT
+ * Permet de modifier un film
+ */
+server.put('/api/films/:id', async (req, res)=>{
+
+    try {
+
+        const id = req.params.id;
+        const filmModifie = req.body;
+
+       
+        if(filmModifie.titre == undefined || filmModifie.genres == undefined || filmModifie.description == undefined || filmModifie.annee == undefined || filmModifie.realisation == undefined || filmModifie.titreVignette == undefined ) {
+
+            res.statusCode = 400;
+            return res.json({ message: 'Veuillez remplir les informations.' });
+        }
+
+        await db.collection('films').doc(id).update(filmModifie);
+
+        res.statusCode = 200;
+        res.json({ message : 'Le film a été modifié' });
+
+    } catch (e) {
+
+        res.statusCode = 500;
+        res.json({ message: 'Erreur de modification' });
+
+    }
+});
+
+/**
+ * @method DELETE
+ * Permet de supprimer un film
+ */
+server.delete('/api/films/:id', async (req, res)=>{
+
+    try {
+
+        const id = req.params.id;
+
+        const resultat = await db.collection('films').doc(id).delete();
+
+        res.statusCode = 200;
+        res.json({ message : 'Le film a été supprimée' });
+
+    } catch (e) {
+
+        res.statusCode = 500;
+        res.json({ message: 'Erreur lors de la suppression' });
+
+    }
+});
+
 
 
 
