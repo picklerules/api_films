b279f29cc7324b8e38e69dfca44192ac1084b1c9
picklerules <136508588+picklerules@users.noreply.github.com>
2024-02-22 12:21:49 -0500

    installer cors

--- app.js
+++ app.js
@@ -1,101 +1,87 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const mustacheExpress = require('mustache-express');
-const db = require('./config/db.js');
-const { check, validationResult } = require('express-validator');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const mustacheExpress = require("mustache-express");
+const cors = require('cors');
+const db = require("./config/db.js");
+const { check, validationResult } = require("express-validator");
 
 //configurer la variable env pour protéger les info d'environnement
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-//configurations 
+//configurations
 dotenv.config();
 
 const server = express();
+app.use(cors());
 ///////////////////////
 
 //definir le type de view
-server.set('views', path.join(__dirname, 'views'));
-server.set('view engine', 'mustache');
-server.engine('mustache', mustacheExpress());
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "mustache");
+server.engine("mustache", mustacheExpress());
 
 //middlewares
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, "public")));
 server.use(express.json());
 
 //point d'accès
-//Récupérer tous les films 
-server.get('/api/films', async (req, res)=>{
-    
-    try {    
-
+//Récupérer tous les films
+server.get("/api/films", async (req, res) => {
+  try {
     //pour mettre la req dans le url
     console.log(req.query);
-    const direction = req.query['ordre'] || 'asc';  
-    const tri = req.query['tri'] || 'titre';
-    
+    const direction = req.query["ordre"] || "asc";
+    const tri = req.query["tri"] || "titre";
 
-    const donneesRef = await db.collection('films').orderBy(tri, direction).get(); 
+    const donneesRef = await db
+      .collection("films")
+      .orderBy(tri, direction)
+      .get();
     const donneesFinale = [];
-    
-    donneesRef.forEach((doc)=>{
 
-        donneesFinale.push(doc.data());
-
-    })
+    donneesRef.forEach((doc) => {
+      donneesFinale.push(doc.data());
+    });
 
     res.statusCode = 200;
     res.json(donneesFinale);
-
-    } catch (e) {
-
-        res.statusCode = 500;
-        res.json({ message: 'Erreur serveur' });
-    }
-
+  } catch (e) {
+    res.statusCode = 500;
+    res.json({ message: "Erreur serveur" });
+  }
 });
-
 
 /**
  * @method GET
- * @param id 
+ * @param id
  * Permet d'accéder à un film selon son id
  */
-server.get('/api/films/:id', async (req, res) =>{
+server.get("/api/films/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
 
-    try {
-        
-        const id = req.params.id;
-        
-        const film = await db.collection('films').doc(id).get();
+    const film = await db.collection("films").doc(id).get();
 
-        if(!film.exists) {
-    
-            res.statusCode = 404;
-            res.json({ message: 'Film non trouvé' });
-
-        } else {
-
-        res.statusCode = 200;
-        res.json(film.data());
-
-        }
-
-    } catch (e) {
-
-        res.statusCode = 500;
-        res.json({message:'Erreur lors de la récupération du film'})
-
+    if (!film.exists) {
+      res.statusCode = 404;
+      res.json({ message: "Film non trouvé" });
+    } else {
+      res.statusCode = 200;
+      res.json(film.data());
     }
-})
-  
+  } catch (e) {
+    res.statusCode = 500;
+    res.json({ message: "Erreur lors de la récupération du film" });
+  }
+});
 
 /**
  * @method POST
  * Permet d'initialiser la base de donnée
  */
 // server.post('/api/films/initialiser',(req,res)=>{
-   
+
 //     const donneesTest = require('./data/filmsTest.js');
 
 //     donneesTest.forEach(async(element)=>{
@@ -107,11 +93,11 @@ server.get('/api/films/:id', async (req, res) =>{
 // });
 
 // /**
-//  * @method POST 
+//  * @method POST
 //  * Permet d'initialiser la base de donnée
 //  */
 // server.post('/api/utilisateurs/initialiser',(req,res)=>{
-   
+
 //     const donneesTest = require('./data/utilisateurTest.js');
 
 //     donneesTest.forEach(async(element)=>{
@@ -122,249 +108,258 @@ server.get('/api/films/:id', async (req, res) =>{
 //     res.json({ message : 'La base de donnée a été initialisée avec les utilisateurs.' });
 // });
 
-
 /**
  * @method POST
  * Permet d'ajouter un film
  */
-server.post('/api/films', 
-[check('titre').escape().trim().notEmpty().isString().exists(),
-check('genres').escape().trim().notEmpty().isArray().exists(),
-check('description').escape().trim().notEmpty().isString().exists(),
-check('annee').escape().trim().notEmpty().isString().exists(),
-check('realisation').escape().trim().notEmpty().isString().exists(),
-check('titreVignette').escape().trim().notEmpty().isString().exists(),
-check('commentaires').escape().trim().notEmpty().isString().isLength({max:200}).exists()],
- async (req, res)=>{
-    
+server.post(
+  "/api/films",
+  [
+    check("titre").escape().trim().notEmpty().isString().exists(),
+    check("genres").escape().trim().notEmpty().isArray().exists(),
+    check("description").escape().trim().notEmpty().isString().exists(),
+    check("annee").escape().trim().notEmpty().isString().exists(),
+    check("realisation").escape().trim().notEmpty().isString().exists(),
+    check("titreVignette").escape().trim().notEmpty().isString().exists(),
+    check("commentaires")
+      .escape()
+      .trim()
+      .notEmpty()
+      .isString()
+      .isLength({ max: 200 })
+      .exists(),
+  ],
+  async (req, res) => {
     try {
-        //validation des données
-        const validation = validationResult(req);
-        if(validation.errors.length > 0) {
+      //validation des données
+      const validation = validationResult(req);
+      if (validation.errors.length > 0) {
+        res.statusCode = 400;
+        return res.json({ message: "Données non-conformes" });
+      }
 
-            res.statusCode = 400;
-            return res.json({ message: 'Données non-conformes' });
-        }
+      const film = req.body;
 
+      const newFilm = await db.collection("films").add(film);
 
-        const film = req.body;
-
-        const newFilm = await db.collection('films').add(film);
-
-    res.statusCode = 201;
-    res.json({ message : 'Le film a été ajouté', id: newFilm.id, donnees: film});
-
+      res.statusCode = 201;
+      res.json({
+        message: "Le film a été ajouté",
+        id: newFilm.id,
+        donnees: film,
+      });
     } catch (e) {
-
-        res.statusCode = 500;
-        res.json({ message: 'Erreur lors de l\'ajout du film' });
-
+      res.statusCode = 500;
+      res.json({ message: "Erreur lors de l'ajout du film" });
     }
-});
+  }
+);
 
 /**
  * @method POST
  * Permet d'ajouter un utilisateur / inscrire un utilisateur
  */
-server.post('/api/utilisateurs/inscription', 
-[check('courriel').escape().trim().notEmpty().isEmail().normalizeEmail().exists(),
-check('mdp').escape().trim().notEmpty().isLength({min:8, max:20}).isStrongPassword({
-    minLength: 8, 
-    minLowercase:1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1
-}).exists()],
-async (req, res)=>{
-
+server.post(
+  "/api/utilisateurs/inscription",
+  [
+    check("courriel")
+      .escape()
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .normalizeEmail()
+      .exists(),
+    check("mdp")
+      .escape()
+      .trim()
+      .notEmpty()
+      .isLength({ min: 8, max: 20 })
+      .isStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .exists(),
+  ],
+  async (req, res) => {
     try {
-        //validation des données
-        const validation = validationResult(req);
+      //validation des données
+      const validation = validationResult(req);
 
-        if (validation.errors.length > 0) {
-            res.statusCode = 400;
-            return res.json({ message: 'Données non-conformes' });
-        }
+      if (validation.errors.length > 0) {
+        res.statusCode = 400;
+        return res.json({ message: "Données non-conformes" });
+      }
 
-        const {courriel, mdp} = req.body;
+      const { courriel, mdp } = req.body;
 
-        //vérifier si l'utilisateur existe déjà
-        const docRef = await db.collection('utilisateurs').where('courriel', '==', courriel).get();
+      //vérifier si l'utilisateur existe déjà
+      const docRef = await db
+        .collection("utilisateurs")
+        .where("courriel", "==", courriel)
+        .get();
 
-        const utilisateurs = [];
+      const utilisateurs = [];
 
-        docRef.forEach((doc)=>{
-            utilisateurs.push(doc.data());
-        });
+      docRef.forEach((doc) => {
+        utilisateurs.push(doc.data());
+      });
 
-        if (utilisateurs.length > 0) {
-            res.statusCode = 400;
-            return res.json({ message: 'Ce courriel existe déjà' }); 
-        }
+      if (utilisateurs.length > 0) {
+        res.statusCode = 400;
+        return res.json({ message: "Ce courriel existe déjà" });
+      }
 
-        const newUtilisateur = { courriel, mdp };
-        await db.collection('utilisateurs').add(newUtilisateur);
+      const newUtilisateur = { courriel, mdp };
+      await db.collection("utilisateurs").add(newUtilisateur);
 
-        delete newUtilisateur.mdp;
+      delete newUtilisateur.mdp;
 
-        res.statusCode = 200;
-        res.json({ message : 'L\'utilisateur a été ajouté', newUtilisateur: newUtilisateur});
-
+      res.statusCode = 200;
+      res.json({
+        message: "L'utilisateur a été ajouté",
+        newUtilisateur: newUtilisateur,
+      });
     } catch (e) {
-
-        res.statusCode = 500;
-        res.json({ message: 'Erreur lors de l\'ajout de l\'utilisateur' });
-
+      res.statusCode = 500;
+      res.json({ message: "Erreur lors de l'ajout de l'utilisateur" });
     }
-});
-
+  }
+);
 
 /**
  * @method POST
  * Permet la connexion d'un utilisateur
  */
-server.post('/api/utilisateurs/connexion',
-[check('courriel').escape().trim().notEmpty().exists(),
-check('mdp').escape().trim().notEmpty().exists()],
+server.post(
+  "/api/utilisateurs/connexion",
+  [
+    check("courriel").escape().trim().notEmpty().exists(),
+    check("mdp").escape().trim().notEmpty().exists(),
+  ],
 
-async (req, res)=>{
+  async (req, res) => {
+    try {
+      const validation = validationResult(req);
 
-  try {
-
-    const validation = validationResult(req);
-
-    if  (validation.errors.length > 0) {
+      if (validation.errors.length > 0) {
         res.statusCode = 400;
-        return res.json({ message: 'Veuillez remplir les champs' });
-    }
+        return res.json({ message: "Veuillez remplir les champs" });
+      }
 
+      const { courriel, mdp } = req.body;
 
-    const {courriel, mdp} = req.body;
+      //vérifier si le courriel de l'utilisateur existe dans la DB
+      const docRef = await db
+        .collection("utilisateurs")
+        .where("courriel", "==", courriel)
+        .get();
 
-    //vérifier si le courriel de l'utilisateur existe dans la DB
-    const docRef = await db.collection('utilisateurs').where('courriel', '==', courriel).get();
+      const utilisateurs = [];
 
-    const utilisateurs = [];
-
-    docRef.forEach((doc)=>{
+      docRef.forEach((doc) => {
         utilisateurs.push(doc.data());
-    });
+      });
 
-    //comparer le mdp avec la DB
-    if(utilisateurs.length > 0) {
-
+      //comparer le mdp avec la DB
+      if (utilisateurs.length > 0) {
         //recuperer l'utilisateur connecté
         const utilisateurAValider = utilisateurs[0];
 
-        if(mdp == utilisateurAValider.mdp) {
-        //on retourne les infos de l'utilisateur sans le mot de passe
-        delete utilisateurAValider.mdp;
-        res.statusCode = 200;
-        res.json({ message:'Vous êtes connecté', utilisateurAValider});
-
+        if (mdp == utilisateurAValider.mdp) {
+          //on retourne les infos de l'utilisateur sans le mot de passe
+          delete utilisateurAValider.mdp;
+          res.statusCode = 200;
+          res.json({ message: "Vous êtes connecté", utilisateurAValider });
         } else {
-
-            res.statusCode = 400;
-            res.json({ message: 'Mot de passe incorrect' });
-
+          res.statusCode = 400;
+          res.json({ message: "Mot de passe incorrect" });
         }
-
-    } else {
-
+      } else {
         res.statusCode = 400;
-        res.json({ message: 'Utilisateur non trouvé' });
-
+        res.json({ message: "Utilisateur non trouvé" });
+      }
+    } catch (e) {
+      res.statusCode = 500;
+      res.json({ message: "Erreur lors de la connexion" });
     }
-
-  } catch (e) {
-
-    res.statusCode = 500;
-    res.json({ message: 'Erreur lors de la connexion' });
-
   }
-
-
-
-
-})
+);
 
 /**
  * @method PUT
  * Permet de modifier un film
  */
-server.put('/api/films/:id', 
-[check('titre').optional().escape().trim().notEmpty().isString(),
-check('genres').optional().escape().trim().notEmpty().isArray(),
-check('description').optional().escape().trim().notEmpty().isString(),
-check('annee').optional().escape().trim().notEmpty().isString(),
-check('realisation').optional().escape().trim().notEmpty().isString(),
-check('titreVignette').optional().escape().trim().notEmpty().isString(),
-check('commentaires').optional().escape().trim().notEmpty().isString().isLength({max:200})], 
-async (req, res)=>{
-
+server.put(
+  "/api/films/:id",
+  [
+    check("titre").optional().escape().trim().notEmpty().isString(),
+    check("genres").optional().escape().trim().notEmpty().isArray(),
+    check("description").optional().escape().trim().notEmpty().isString(),
+    check("annee").optional().escape().trim().notEmpty().isString(),
+    check("realisation").optional().escape().trim().notEmpty().isString(),
+    check("titreVignette").optional().escape().trim().notEmpty().isString(),
+    check("commentaires")
+      .optional()
+      .escape()
+      .trim()
+      .notEmpty()
+      .isString()
+      .isLength({ max: 200 }),
+  ],
+  async (req, res) => {
     try {
+      //validation des données
+      const validation = validationResult(req);
 
-        //validation des données
-        const validation = validationResult(req);
+      if (validation.errors.length > 0) {
+        res.statusCode = 400;
+        return res.json({ message: "Données non-conformes" });
+      }
 
-        if (validation.errors.length > 0) {
+      const id = req.params.id;
+      const filmModifie = req.body;
 
-            res.statusCode = 400;
-            return res.json({ message: 'Données non-conformes' });
-        }
+      await db.collection("films").doc(id).update(filmModifie);
 
-        const id = req.params.id;
-        const filmModifie = req.body;
-
-
-        await db.collection('films').doc(id).update(filmModifie);
-
-        res.statusCode = 200;
-        res.json({ message : 'Le film a été modifié' });
-
+      res.statusCode = 200;
+      res.json({ message: "Le film a été modifié" });
     } catch (e) {
-
-        res.statusCode = 500;
-        res.json({ message: 'Erreur de modification' });
-
+      res.statusCode = 500;
+      res.json({ message: "Erreur de modification" });
     }
-});
+  }
+);
 
 /**
  * @method DELETE
  * Permet de supprimer un film
  */
-server.delete('/api/films/:id', async (req, res)=>{
+server.delete("/api/films/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
 
-    try {
+    const resultat = await db.collection("films").doc(id).delete();
 
-        const id = req.params.id;
-
-        const resultat = await db.collection('films').doc(id).delete();
-
-        res.statusCode = 200;
-        res.json({ message : 'Le film a été supprimée' });
-
-    } catch (e) {
-
-        res.statusCode = 500;
-        res.json({ message: 'Erreur lors de la suppression' });
-
-    }
+    res.statusCode = 200;
+    res.json({ message: "Le film a été supprimée" });
+  } catch (e) {
+    res.statusCode = 500;
+    res.json({ message: "Erreur lors de la suppression" });
+  }
 });
-
-
 
 //DOIT ETRE LA DERNIÈRE, gestion des erreurs (page 404 ou requête non trouvée)
-server.use((req, res)=>{
-    res.statusCode = 404;
-    res.render('404', { url: req.url });
-})
-
-//à la toute fin
-server.listen(process.env.PORT, ()=>{
-    console.log('Le serveur a démarré');
+server.use((req, res) => {
+  res.statusCode = 404;
+  res.render("404", { url: req.url });
 });
 
+//à la toute fin
+server.listen(process.env.PORT, () => {
+  console.log("Le serveur a démarré");
+});
 
 // npm run dev  pour runner mon script
